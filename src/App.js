ff4423f90d2855290abef316f7255afccf13c280
hoshimaro <89444685+khoshi-higashi@users.noreply.github.com>
2022-02-08 10:45:50 +0900

    Add material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 import React, { useState } from "react";
+// import Button from "@mui/material/Button";
+// import InputLabel from "@mui/material/InputLabel";
+// import { FormControl } from "@mui/material/FormControl";
+import { Input, FormControl, InputLabel, Button } from "@mui/material";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -7,37 +11,66 @@ function App() {
     "Take the rubbish out",
   ]);
   const [input, setInput] = useState("");
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [body, setBody] = useState("");
+  const [titles, setTitles] = useState([]);
+  const [author, setAuthor] = useState([]);
+  const [body, setBody] = useState([]);
 
-  console.log("🔫", input);
+  // console.log("🔫", input);
 
   const addTodo = (event) => {
+    event.preventDefault(); // will stop the REFRESH
     console.log("👽", "I'm working");
     setTodos([...todos, input]);
+    setInput(""); // clear up the input after clicking add todo button
+    console.log(todos);
   };
 
   const addTitle = (event) => {
+    event.preventDefault(); // will stop the REFRESH
     console.log("👽", "I'm working");
-    setTodos([...todos, input]);
+    setTitles([...titles, input]);
+    setInput(""); // clear up the input after clicking add todo button
+    console.log(titles);
   };
 
   const addAuthor = (event) => {
+    event.preventDefault(); // will stop the REFRESH
     console.log("👽", "I'm working");
-    setTodos([...todos, input]);
+    setAuthor([...author, input]);
+    setInput(""); // clear up the input after clicking add todo button
+    console.log(author);
   };
 
   const addBody = (event) => {
+    event.preventDefault(); // will stop the REFRESH
     console.log("👽", "I'm working");
-    setTodos([...todos, input]);
+    setBody([...body, input]);
+    setInput(""); // clear up the input after clicking add todo button
+    console.log(body);
   };
 
   return (
     <div className="App">
       <h1>Hello Clever Programmers 🚀!</h1>
-      <input onChange={(event) => setInput(event.target.value)} value={input} />
-      <button onClick={addTodo}>Add ToDo</button>
+      <form>
+        <FormControl>
+          <InputLabel>✅ Write a Todo</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+        </FormControl>
+
+        <Button
+          type="submit"
+          onClick={addTodo}
+          variant="contained"
+          color="primary"
+          disabled={!input}
+        >
+          Add ToDo
+        </Button>
+      </form>
       <ul>
         {todos.map((todo) => (
           <li>{todo}</li>
@@ -45,15 +78,63 @@ function App() {
       </ul>
 
       <h1>Book highlight submission site 📚</h1>
-      <input value={title} onChange={(event) => setTitle(event.target.value)} />
-      <button onClick={addTitle}>Add Title</button>
-      <input
-        value={author}
-        onChange={(event) => setAuthor(event.target.value)}
-      />
-      <button onClick={addAuthor}>Add Author</button>
-      <input value={body} onChange={(event) => setBody(event.target.value)} />
-      <button onClick={addBody}>Add Body</button>
+      <form>
+        <FormControl>
+          <InputLabel>✅ Write a Title</InputLabel>
+          <Input
+            value={titles}
+            onChange={(event) => setTitles(event.target.value)}
+          />
+        </FormControl>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={addTitle}
+          disabled={!titles}
+        >
+          Add Title
+        </Button>
+        <ul>
+          {titles.map((title) => (
+            <li>{title}</li>
+          ))}
+        </ul>
+
+        <FormControl>
+          <InputLabel>✅ Write a Author</InputLabel>
+          <Input
+            value={author}
+            onChange={(event) => setAuthor(event.target.value)}
+          />
+        </FormControl>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={addAuthor}
+          disabled={!author}
+        >
+          Add Author
+        </Button>
+
+        <FormControl>
+          <InputLabel>✅ Write a Body</InputLabel>
+          <Input
+            value={body}
+            onChange={(event) => setBody(event.target.value)}
+          />
+        </FormControl>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={addBody}
+          disabled={!body}
+        >
+          Add Body
+        </Button>
+      </form>
     </div>
   );
 }
