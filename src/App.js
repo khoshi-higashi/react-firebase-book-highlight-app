0573565fd302711935bdc65d9d8b4c14d8d0d267
hoshimaro <89444685+khoshi-higashi@users.noreply.github.com>
2022-02-08 15:46:08 +0900

    books.map が機能していない

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import { Input, FormControl, InputLabel, Button } from "@mui/material";
 import Todo from "./Todo";
+import Book from "./Book";
 import { db } from "./firebase";
 import {
   collection,
@@ -15,8 +16,35 @@ import {
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputAuthor, setInputAuthor] = useState("");
+  const [inputBody, setInputBody] = useState("");
+  const [titles, setTitles] = useState([]);
+  const [author, setAuthor] = useState([]);
+  const [body, setBody] = useState([]);
+  const [books, setBooks] = useState("");
 
   const todosCollectionRef = collection(db, "todos");
+  const booksCollectionRef = collection(db, "books");
+
+  // When the app loads, we need to listen to the database and fetch new todos as they get added/removed
+  useEffect(() => {
+    // This code here... fires when the app.js loads
+    onSnapshot(
+      query(booksCollectionRef, orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setBooks(
+          snapshot.docs.map((doc) => {
+            console.log(doc);
+            return {
+              id: doc.id,
+              book: doc.data().book,
+            };
+          })
+        );
+      }
+    );
+  }, []);
 
   // When the app loads, we need to listen to the database and fetch new todos as they get added/removed
   useEffect(() => {
@@ -36,49 +64,71 @@ function App() {
     );
   }, []);
 
-  const [titles, setTitles] = useState([]);
-  const [author, setAuthor] = useState([]);
-  const [body, setBody] = useState([]);
-
-  // console.log("🔫", input);
-
   const addTodo = (event) => {
     event.preventDefault(); // will stop the REFRESH
-
     addDoc(collection(db, "todos"), {
       todo: input,
       timestamp: serverTimestamp(),
     });
-
     setInput(""); // clear up the input after clicking add todo button
   };
 
-  const addTitle = (event) => {
+  const addBook = (event) => {
     event.preventDefault(); // will stop the REFRESH
-    console.log("👽", "I'm working");
-    setTitles([...titles, input]);
-    setInput(""); // clear up the input after clicking add todo button
-    console.log(titles);
-  };
-
-  const addAuthor = (event) => {
-    event.preventDefault(); // will stop the REFRESH
-    console.log("👽", "I'm working");
-    setAuthor([...author, input]);
-    setInput(""); // clear up the input after clicking add todo button
-    console.log(author);
-  };
-
-  const addBody = (event) => {
-    event.preventDefault(); // will stop the REFRESH
-    console.log("👽", "I'm working");
-    setBody([...body, input]);
-    setInput(""); // clear up the input after clicking add todo button
-    console.log(body);
+    addDoc(collection(db, "books"), {
+      title: inputTitle,
+      author: inputAuthor,
+      body: inputBody,
+      timestamp: serverTimestamp(),
+    });
+    setInputTitle(""); // clear up the input after clicking add todo button
+    setInputAuthor(""); // clear up the input after clicking add todo button
+    setInputBody(""); // clear up the input after clicking add todo button
   };
 
   return (
     <div className="App">
+      <h1>Book highlight submission site 📚</h1>
+      <form>
+        <FormControl>
+          <InputLabel>✅ Write a Title</InputLabel>
+          <Input
+            value={inputTitle}
+            onChange={(event) => setInputTitle(event.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel>✅ Write a Author</InputLabel>
+          <Input
+            value={inputAuthor}
+            onChange={(event) => setInputAuthor(event.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel>✅ Write Body</InputLabel>
+          <Input
+            value={inputBody}
+            onChange={(event) => setInputBody(event.target.value)}
+          />
+        </FormControl>
+
+        <Button
+          type="submit"
+          onClick={addBook}
+          variant="contained"
+          color="primary"
+          disabled={!inputTitle || !inputAuthor || !inputBody}
+        >
+          Add Highlight
+        </Button>
+      </form>
+
+      <ul>
+        {books.map((book) => (
+          <Book book={book} />
+        ))}
+      </ul>
+
       <h1>Hello Clever Programmers 🚀!</h1>
       <form>
         <FormControl>
@@ -104,65 +154,6 @@ function App() {
           <Todo todo={todo} />
         ))}
       </ul>
-
-      <h1>Book highlight submission site 📚</h1>
-      <form>
-        <FormControl>
-          <InputLabel>✅ Write a Title</InputLabel>
-          <Input
-            value={titles}
-            onChange={(event) => setTitles(event.target.value)}
-          />
-        </FormControl>
-        <Button
-          variant="contained"
-          color="primary"
-          type="submit"
-          onClick={addTitle}
-          disabled={!titles}
-        >
-          Add Title
-        </Button>
-        <ul>
-          {titles.map((title) => (
-            <li>{title}</li>
-          ))}
-        </ul>
-
-        <FormControl>
-          <InputLabel>✅ Write a Author</InputLabel>
-          <Input
-            value={author}
-            onChange={(event) => setAuthor(event.target.value)}
-          />
-        </FormControl>
-        <Button
-          variant="contained"
-          color="primary"
-          type="submit"
-          onClick={addAuthor}
-          disabled={!author}
-        >
-          Add Author
-        </Button>
-
-        <FormControl>
-          <InputLabel>✅ Write a Body</InputLabel>
-          <Input
-            value={body}
-            onChange={(event) => setBody(event.target.value)}
-          />
-        </FormControl>
-        <Button
-          variant="contained"
-          color="primary"
-          type="submit"
-          onClick={addBody}
-          disabled={!body}
-        >
-          Add Body
-        </Button>
-      </form>
     </div>
   );
 }
