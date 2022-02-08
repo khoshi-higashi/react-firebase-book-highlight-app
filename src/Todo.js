fac0846240898fc4176d9dc14552072f903a2236
hoshimaro <89444685+khoshi-higashi@users.noreply.github.com>
2022-02-08 11:07:25 +0900

    Todo コンポーネントに List, ListItem, ListItemText を追加

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { List, ListItem, ListItemText } from "@mui/material";
 
 function Todo(props) {
   return (
-    <div>
-      <li>{props.text}</li>
-    </div>
+    <List>
+      <ListItem>
+        <ListItemText primary="Todo" secondary={props.text} />
+      </ListItem>
+    </List>
   );
 }
 
