4767b2a88557e0d577702b84f5e8508b3d218f27
hoshimaro <89444685+khoshi-higashi@users.noreply.github.com>
2022-03-03 10:28:53 +0900

    リファクタリング

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,7 @@
 import React, { useState, useEffect, forwardRef } from "react";
-import {
-  List,
-  ListItem,
-  ListItemText,
-  Button,
-  Modal,
-  Input,
-  FormControl,
-  InputLabel,
-} from "@mui/material";
+import { List, ListItem, Modal } from "@mui/material";
 import { db } from "../firebase";
-import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { deleteDoc, doc } from "firebase/firestore";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import Box from "@mui/material/Box";
 import "../css/Book.css";
@@ -18,39 +9,11 @@ import "../css/Book.css";
 const Book = forwardRef(
   ({ user, book, setSelectedItem, selectedItem }, ref) => {
     const [open, setOpen] = useState(false);
-    const [inputTitle, setInputTitle] = useState("");
-    const [inputAuthor, setInputAuthor] = useState("");
-    const [inputBody, setInputBody] = useState("");
     const [activeBook, setActiveBook] = useState(0);
 
     useEffect(() => {
       setActiveBook(selectedItem);
     }, [selectedItem, activeBook]);
-
-    const UpdateBook = () => {
-      if (inputTitle !== "") {
-        updateDoc(doc(db, "books", book.id), {
-          title: inputTitle,
-        });
-      }
-      if (inputAuthor !== "") {
-        updateDoc(doc(db, "books", book.id), {
-          author: inputAuthor,
-        });
-      }
-      if (inputBody !== "") {
-        updateDoc(doc(db, "books", book.id), {
-          body: inputBody,
-        });
-      }
-      updateDoc(doc(db, "books", book.id), {
-        user: user.displayName,
-      });
-      setInputTitle("");
-      setInputAuthor("");
-      setInputBody("");
-      setOpen(false);
-    };
 
     const returnTop = () => {
       window.scrollTo({
